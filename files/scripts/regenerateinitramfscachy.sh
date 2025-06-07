4528b8f371ad77db86f0a0aea07f526d64a1a5c9
Celeste Peláez <KernelFreeze@users.noreply.github.com>
2025-06-07 05:13:49 -0500

    ✨ (initramfs): add script and module for CachyOS kernel initramfs regeneration
    Introduce regenerateinitramfscachy.sh to automate initramfs image
    regeneration for the CachyOS kernel, ensuring correct dracut invocation
    and permissions. Add final-modules-cachy.yml to integrate this script
    into the build process. Update recipe-hyprland.yml to include the new
    module, enabling reliable initramfs creation for custom kernel builds.
    This change improves system compatibility and automates a previously
    manual step.

---
+++ files/scripts/regenerateinitramfscachy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+rpm-ostree cliwrap install-to-root /
+
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-cachyos-lto-(\d+\.\d+\.\d+)' | sed -E 's/kernel-cachyos-lto-//')"
+/usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
